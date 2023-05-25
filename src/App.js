99fdc1f72bd150b4daf73abfb32b3f37c58fae14
petertrai <petertrai@gmail.com>
2023-05-25 16:54:55 -0700

    add

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
-export default function App () {
+
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movies, setMovies] = useState([]);
 
@@ -12,6 +16,7 @@ export default function App () {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovies(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movies' slice of state
         })
@@ -29,8 +34,10 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Routes>
+        <Route path='/' element={<MovieList movies={movies}/>} />
+        <Route path='movies/:id' element={<Movie />} />
+      </Routes>
     </div>
   );
 }
--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 export default function MovieList(props) {
+  const navigate = useNavigate();
+  const handleClick = id => () => {
+    navigate(`movies/${id}`)
+  }
+  
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails handleClick={handleClick(movie.id)} key={movie.id} movie={movie} />
       ))}
     </div>
   );
@@ -12,9 +18,9 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
-
+  const { handleClick } = props
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={handleClick}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
